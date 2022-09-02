a1d4a8396ec6e9ad3333f82e7a42e9ecc85f678d
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-09-02 11:27:51 +0300

    Create a function to get Movies from the base URL

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const base_URL = "https://api.themoviedb.org/3";
 const api_URL = `${base_URL}/discover/movie?sort_by=popularity.desc&${api_KEY}`;
 const img_URL = "https://image.tmdb.org/t/p/w500";
 const searchURL = `${base_URL}/search/movie?${api_KEY}`;
+
+function getMovies(url) {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      displayMovies(data.results);
+      console.log(data);
+    });
+}
