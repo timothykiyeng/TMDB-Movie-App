368537a4ad5f8fd020fd6841e605be4d5afc0b05
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-09-02 11:26:38 +0300

    declared variables to hold the dynamic URLs

---
+++ index.js
@@ -0,0 +1,5 @@
+const api_KEY = "api_key=15e383204c1b8a09dbfaaa4c01ed7e17";
+const base_URL = "https://api.themoviedb.org/3";
+const api_URL = `${base_URL}/discover/movie?sort_by=popularity.desc&${api_KEY}`;
+const img_URL = "https://image.tmdb.org/t/p/w500";
+const searchURL = `${base_URL}/search/movie?${api_KEY}`;
